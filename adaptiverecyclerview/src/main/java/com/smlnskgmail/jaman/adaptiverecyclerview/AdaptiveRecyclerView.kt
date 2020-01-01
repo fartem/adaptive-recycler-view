d17be973d42fccf05612704f1fd7e1923e3b6bf2
Artem <jaman.smlnsk@gmail.com>
2020-01-01 15:54:06 +0300

    2020-01-01 Version 0.1.0: Updated project structure

---
+++ adaptiverecyclerview/src/main/java/com/smlnskgmail/jaman/adaptiverecyclerview/AdaptiveRecyclerView.kt
@@ -0,0 +1,26 @@
+package com.smlnskgmail.jaman.adaptiverecyclerview
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.recyclerview.widget.RecyclerView
+
+class AdaptiveRecyclerView : RecyclerView {
+
+    constructor(context: Context) : super(context)
+
+    constructor(
+        context: Context,
+        attrs: AttributeSet?
+    ) : super(context, attrs)
+
+    constructor(
+        context: Context,
+        attrs: AttributeSet?,
+        defStyleAttr: Int
+    ) : super(
+        context,
+        attrs,
+        defStyleAttr
+    )
+
+}
